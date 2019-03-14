099c11bb4febaf194ae3260c56666fd0f3d0d68f
Keith DeSimini <kdesimini@bsu.edu>
2019-03-14 16:05:44 -0400

    Update Create_Database_Dev.sql

--- Resources/Create_Database_Dev.sql
+++ Resources/Create_Database_Dev.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: data.mildvariety.club
--- Generation Time: Jan 17, 2019 at 02:56 PM
+-- Generation Time: Mar 14, 2019 at 12:56 PM
 -- Server version: 5.6.34-log
 -- PHP Version: 7.1.22
 
@@ -21,6 +21,20 @@ SET time_zone = "+00:00";
 --
 -- Database: `test284829`
 --
+CREATE DATABASE IF NOT EXISTS `test284829` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `test284829`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buses`
+--
+
+DROP TABLE IF EXISTS `buses`;
+CREATE TABLE `buses` (
+  `busIdentifier` varchar(32) NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -28,6 +42,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `Entries`
 --
 
+DROP TABLE IF EXISTS `Entries`;
 CREATE TABLE `Entries` (
   `boarded` int(11) NOT NULL,
   `stop` varchar(255) NOT NULL DEFAULT '',
@@ -35,12 +50,59 @@ CREATE TABLE `Entries` (
   `date` date NOT NULL,
   `loop` varchar(255) NOT NULL,
   `driver` varchar(255) NOT NULL,
+  `id` int(11) NOT NULL,
+  `leftBehind` int(255) DEFAULT '0',
+  `busIdentifier` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `loops`
+--
+
+DROP TABLE IF EXISTS `loops`;
+CREATE TABLE `loops` (
+  `loops` varchar(64) NOT NULL,
   `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stops`
+--
+
+DROP TABLE IF EXISTS `stops`;
+CREATE TABLE `stops` (
+  `stops` varchar(32) NOT NULL,
+  `loops` varchar(256) NOT NULL,
+  `displayOrder` int(11) NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(256) NOT NULL,
+  `lastname` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `buses`
+--
+ALTER TABLE `buses`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `Entries`
@@ -49,13 +111,55 @@ ALTER TABLE `Entries`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `loops`
+--
+ALTER TABLE `loops`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `stops`
+--
+ALTER TABLE `stops`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `buses`
+--
+ALTER TABLE `buses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `Entries`
 --
 ALTER TABLE `Entries`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `loops`
+--
+ALTER TABLE `loops`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `stops`
+--
+ALTER TABLE `stops`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 

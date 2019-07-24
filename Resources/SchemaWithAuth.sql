854570261beca69a9dcf8196c19fefdad03c1955
Keith <34429274+kdesimini@users.noreply.github.com>
2019-07-24 13:55:21 -0500

    New Schema with Authentication System

---
+++ Resources/SchemaWithAuth.sql
@@ -0,0 +1,305 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jul 24, 2019 at 06:54 PM
+-- Server version: 5.6.42
+-- PHP Version: 7.0.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- Database: `buslogs_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buses`
+--
+
+CREATE TABLE `buses` (
+  `id` int(11) NOT NULL,
+  `busIdentifier` varchar(255) DEFAULT NULL,
+  `is_deleted` bit(1) NOT NULL DEFAULT b'0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `entries`
+--
+
+CREATE TABLE `entries` (
+  `boarded` int(11) DEFAULT NULL,
+  `stop` int(11) DEFAULT NULL,
+  `t_stamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `date_added` date DEFAULT NULL,
+  `loop` int(11) DEFAULT NULL,
+  `driver` int(11) DEFAULT NULL,
+  `id` int(11) NOT NULL,
+  `left_behind` int(11) DEFAULT NULL,
+  `bus_identifier` int(11) DEFAULT NULL,
+  `is_deleted` bit(1) DEFAULT b'0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `loops`
+--
+
+CREATE TABLE `loops` (
+  `id` int(11) NOT NULL,
+  `loops` varchar(255) DEFAULT NULL,
+  `is_deleted` bit(1) NOT NULL DEFAULT b'0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stops`
+--
+
+CREATE TABLE `stops` (
+  `id` int(11) NOT NULL,
+  `stops` varchar(255) DEFAULT NULL,
+  `is_deleted` bit(1) DEFAULT b'0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stop_loop`
+--
+
+CREATE TABLE `stop_loop` (
+  `id` int(11) NOT NULL,
+  `loop` int(11) DEFAULT NULL,
+  `stop` int(11) DEFAULT NULL,
+  `displayOrder` int(11) DEFAULT NULL,
+  `is_deleted` bit(1) NOT NULL DEFAULT b'0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ul_blocked_ips`
+--
+
+CREATE TABLE `ul_blocked_ips` (
+  `ip` varchar(39) CHARACTER SET ascii NOT NULL,
+  `block_expires` varchar(26) CHARACTER SET ascii NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ul_log`
+--
+
+CREATE TABLE `ul_log` (
+  `timestamp` varchar(26) CHARACTER SET ascii NOT NULL,
+  `action` varchar(20) CHARACTER SET ascii NOT NULL,
+  `comment` varchar(255) CHARACTER SET ascii NOT NULL DEFAULT '',
+  `user` varchar(400) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `ip` varchar(39) CHARACTER SET ascii NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ul_logins`
+--
+
+CREATE TABLE `ul_logins` (
+  `id` int(11) NOT NULL,
+  `username` varchar(400) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(2048) CHARACTER SET ascii NOT NULL,
+  `date_created` varchar(26) CHARACTER SET ascii NOT NULL,
+  `last_login` varchar(26) CHARACTER SET ascii NOT NULL,
+  `block_expires` varchar(26) CHARACTER SET ascii NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ul_nonces`
+--
+
+CREATE TABLE `ul_nonces` (
+  `code` varchar(100) CHARACTER SET ascii NOT NULL,
+  `action` varchar(850) CHARACTER SET ascii NOT NULL,
+  `nonce_expires` varchar(26) CHARACTER SET ascii NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ul_sessions`
+--
+
+CREATE TABLE `ul_sessions` (
+  `id` varchar(128) CHARACTER SET ascii NOT NULL DEFAULT '',
+  `data` blob NOT NULL,
+  `session_expires` varchar(26) CHARACTER SET ascii NOT NULL,
+  `lock_expires` varchar(26) CHARACTER SET ascii NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(255) DEFAULT NULL,
+  `lastname` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `is_deleted` bit(1) DEFAULT b'0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `buses`
+--
+ALTER TABLE `buses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `entries`
+--
+ALTER TABLE `entries`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `stop` (`stop`),
+  ADD KEY `loop` (`loop`),
+  ADD KEY `driver` (`driver`),
+  ADD KEY `bus_identifier` (`bus_identifier`);
+
+--
+-- Indexes for table `loops`
+--
+ALTER TABLE `loops`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `stops`
+--
+ALTER TABLE `stops`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `stop_loop`
+--
+ALTER TABLE `stop_loop`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `loop` (`loop`),
+  ADD KEY `stop` (`stop`);
+
+--
+-- Indexes for table `ul_blocked_ips`
+--
+ALTER TABLE `ul_blocked_ips`
+  ADD PRIMARY KEY (`ip`);
+
+--
+-- Indexes for table `ul_logins`
+--
+ALTER TABLE `ul_logins`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`(255));
+
+--
+-- Indexes for table `ul_nonces`
+--
+ALTER TABLE `ul_nonces`
+  ADD PRIMARY KEY (`code`),
+  ADD UNIQUE KEY `action` (`action`(255));
+
+--
+-- Indexes for table `ul_sessions`
+--
+ALTER TABLE `ul_sessions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `buses`
+--
+ALTER TABLE `buses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `entries`
+--
+ALTER TABLE `entries`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `loops`
+--
+ALTER TABLE `loops`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `stops`
+--
+ALTER TABLE `stops`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `stop_loop`
+--
+ALTER TABLE `stop_loop`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `ul_logins`
+--
+ALTER TABLE `ul_logins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `entries`
+--
+ALTER TABLE `entries`
+  ADD CONSTRAINT `entries_ibfk_1` FOREIGN KEY (`stop`) REFERENCES `stops` (`id`),
+  ADD CONSTRAINT `entries_ibfk_2` FOREIGN KEY (`loop`) REFERENCES `loops` (`id`),
+  ADD CONSTRAINT `entries_ibfk_3` FOREIGN KEY (`driver`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `entries_ibfk_4` FOREIGN KEY (`bus_identifier`) REFERENCES `buses` (`id`);
+
+--
+-- Constraints for table `stop_loop`
+--
+ALTER TABLE `stop_loop`
+  ADD CONSTRAINT `stop_loop_ibfk_1` FOREIGN KEY (`loop`) REFERENCES `loops` (`id`),
+  ADD CONSTRAINT `stop_loop_ibfk_2` FOREIGN KEY (`stop`) REFERENCES `stops` (`id`);
+COMMIT;

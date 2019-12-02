c3fe86fcec39a0226e5fe5b2def38e6df40dafb9
Ergin, Huseyin <hergin@bsu.edu>
2019-12-01 23:03:11 -0500

    semi-automation scripts for api

---
+++ Resources/downgrade_api.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+rm -rf api
+mv api_previous api
+rm -rf api_previous
---
+++ Resources/upgrade_api.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+git clone https://github.com/hergin/BusShuttleAPI.git api_new
+rm -rf api_previous
+cp -avr  api api_previous
+currentApiName=api$(date +"_%y%m%d_%I%M%S")
+mv api $currentApiName
+mv api_new/api api
+rm -rf api_new
+cp config-PROD.php api/config.php

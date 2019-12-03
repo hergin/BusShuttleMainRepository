bde5f69d2428d256841d71eaff542c63f135466a
Ergin, Huseyin <hergin@bsu.edu>
2019-12-03 01:40:51 -0500

    separate the test user sql

---
+++ Resources/SampleUser.sql
@@ -0,0 +1,3 @@
+-- Sample Login User
+INSERT INTO `ul_logins` (`id`, `username`, `password`, `date_created`, `last_login`, `block_expires`) VALUES
+(1, 'shuttle', '$2a$11$3gmcQLBRtJIaZjlamlA7euGHAMMa122VPyWViguWAhPOAZbdjr6Ey', '2019-11-21T05:16:09+01:00', '2019-12-02T04:31:38+01:00', '1019-11-21T05:16:09+00:53');
--- Resources/SchemaWithAuth.sql
+++ Resources/SchemaWithAuth.sql
@@ -303,8 +303,3 @@ ALTER TABLE `stop_loop`
   ADD CONSTRAINT `stop_loop_ibfk_1` FOREIGN KEY (`loop`) REFERENCES `loops` (`id`),
   ADD CONSTRAINT `stop_loop_ibfk_2` FOREIGN KEY (`stop`) REFERENCES `stops` (`id`);
 COMMIT;
-
-
---- Sample Login User
-INSERT INTO `ul_logins` (`id`, `username`, `password`, `date_created`, `last_login`, `block_expires`) VALUES
-(1, 'shuttle', '$2a$11$3gmcQLBRtJIaZjlamlA7euGHAMMa122VPyWViguWAhPOAZbdjr6Ey', '2019-11-21T05:16:09+01:00', '2019-12-02T04:31:38+01:00', '1019-11-21T05:16:09+00:53');
